8ffe67615517d94f7bf51ac12784548704d37276
Mahdi <mah.abha8@gmail.com>
2022-05-10 10:11:31 +0200

    issue #31 : documentation et commentaires ajoutés

--- scripts/allen2avgt_import_proj_density.py
+++ scripts/allen2avgt_import_proj_density.py
@@ -56,6 +56,18 @@ def _build_arg_parser():
 
 
 def check_id(parser, args):
+    """
+    Verify if the experiment id is part of the Allen Mouse Brain Connectivity Altas.
+    Read all experiments ids from the Allen Mouse Brain Connectivity Cache.
+    Dowload the Cache files if does not exits or --cache used.
+
+    Parameters
+    ----------
+    parser: argparse.ArgumentParser object
+        Parser.
+    args: argparse namespace
+        Argument list.
+    """
     experiments_path = './utils/cache/allen_mouse_conn_experiments.json'
     manifest_path = './utils/cache/mouse_conn_manifest.json'
 
@@ -72,6 +84,18 @@ def check_id(parser, args):
 
 
 def check_file_exists(parser, args, path):
+    """
+    Verify that output does not exist or that if it exists, -f should be used.
+    If not used, print parser's usage and exit.
+    Parameters
+    ----------
+    parser: argparse.ArgumentParser object
+        Parser.
+    args: argparse namespace
+        Argument list.
+    path: string or path to file
+        Required path to be checked.
+    """
     if os.path.isfile(path) and not args.overwrite:
         parser.error('Output file {} exists. Use -f to force '
                      'overwriting'.format(path))
@@ -83,22 +107,28 @@ def check_file_exists(parser, args, path):
 
 
 def main():
+    # Building argparser
     parser = _build_arg_parser()
     args = parser.parse_args()
 
+    # Verifying experiment id
     check_id(parser, args)
 
+    # Configuring output directory
     args.dir = Path(args.dir)
     args.dir.mkdir(exist_ok=True, parents=True)
 
-    avgt_file = './utils/AVGT.nii.gz'  # todo
+    # AVGT settings
+    avgt_file = './utils/AVGT.nii.gz'  
     avgt_r_mm = 70 / 1e3
     avgt_offset = np.array([-5.675, -8.79448, -8.450335, 0])
 
     mca = MouseConnectivityApi()
 
+    # ROI of the experiment
     roi = pd.DataFrame(mca.get_experiment_detail(args.id)).specimen[0]['stereotaxic_injections'][0]['primary_injection_structure']['acronym']
 
+    # Choosing the downloaded resolution
     if args.res == 100:
         mca_res = mca.VOXEL_RESOLUTION_100_MICRONS
     elif args.res == 50:
@@ -106,57 +136,71 @@ def main():
     elif args.res == 25:
         mca_res = mca.VOXEL_RESOLUTION_25_MICRONS
 
+    # Configuring file names
     nrrd_file = args.dir / f"{args.id}_{roi}_proj_density_{args.res}.nrrd"
     nifti_file = args.dir / f"{args.id}_{roi}_proj_density_{args.res}.nii.gz"
 
+    # Setting up the interpolation method
     interp = 'nearestNeighbor'
     if args.smooth:
         interp = 'bSpline'
         nifti_file = args.dir / f"{args.id}_{roi}_proj_density_{args.res}_{interp}.nii.gz"
 
+    # Verifying if outputs already exist
     check_file_exists(parser, args, nifti_file)
 
+    # Downloading projection density (API)
     mca.download_projection_density(
         nrrd_file,
         experiment_id=args.id,
         resolution=mca_res
         )
 
+    # Loading volume and deleting nrrd tmp file
     allen_vol, header = nrrd.read(nrrd_file)
     os.remove(nrrd_file)
 
+    # Transforming manually to RAS+
     allen_vol = np.moveaxis(allen_vol, (0, 1, 2), (1, 2, 0))
     allen_vol = np.flip(allen_vol, axis=2)
     allen_vol = np.flip(allen_vol, axis=1)
 
+    # Scale to AGVT 
     affine = np.eye(4) * avgt_r_mm
 
+    # Loading allen volume and converting both arrays to float32
     avgt_vol = nib.load(avgt_file).get_fdata().astype(np.float32)
     allen_vol = allen_vol.astype(np.float32)
 
+    # Creating and reshaping ANTsPyx images for registration
+    # Moving : Allen volume
+    # Fixed : AVGT volume
     fixed = ants.from_numpy(avgt_vol).resample_image((164, 212, 158), 1, 0)
     moving = ants.from_numpy(allen_vol).resample_image((164, 212, 158), 1, 0)
 
-    # todo
+    # Loading pre-calculated transformations (ANTsPyx registration)
     transformations = [f'./utils/transformations_allen2avgt/allen2avgt_{args.res}.nii.gz',
                        f'./utils/transformations_allen2avgt/allen2avgtAffine_{args.res}.mat']
 
+    # Applying thoses transformations
     warped_moving = ants.apply_transforms(fixed=fixed,  moving=moving,
                                           transformlist=transformations,
                                           interpolator=interp)
 
+    # Applying a translation to Allen volume (Same origin as AGVT in MI-Brain)
     affine[:, 3] = affine[:, 3] + avgt_offset
 
+    # Converting the warped volume to numpy array
     warped_vol = warped_moving.numpy()
 
+    # Deleting negatives values if bSpline method was used (--smooth)
     if args.smooth:
         negatives_values = warped_vol < 0
         warped_vol[negatives_values] = 0
 
+    # Creating and Saving the Nifti volume
     img = nib.Nifti1Image(warped_vol, affine)
     nib.save(img, nifti_file)
-
-    return nifti_file
 
 
 if __name__ == "__main__":
