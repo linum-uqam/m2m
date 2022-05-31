3cc0efed4c0b9c8c83fc30b1c2caceeeeca070b2
Mahdi <mah.abha8@gmail.com>
2022-05-31 17:36:50 +0200

    issues #55, premiere version des options du script, wip: verifications

---
+++ scripts/allen_crossing_finder.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+import argparse
+from email.policy import default
+import json
+import logging
+
+import os
+from pathlib import Path
+from tabnanny import check
+
+import numpy as np
+import pandas as pd
+
+from allensdk.api.queries.mouse_connectivity_api import MouseConnectivityApi
+from allensdk.api.queries.reference_space_api import ReferenceSpaceApi
+from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
+
+import nibabel as nib
+import nrrd
+import ants
+
+EPILOG = """
+Author : Mahdi
+"""
+
+def _build_arg_parser():
+    p = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                epilog=EPILOG, description=__doc__)
+    p.add_argument('--red', nargs=3, type=int, required=True,
+                   help='MI-Brain voxels coordinates of the first experiment.\n'
+                        'First experiment will be colored in red.')
+    p.add_argument('--green', nargs=3, type=int, required=True,
+                   help='MI-Brain voxels coordinates of the second experiment.\n'
+                        'Second experiment will be colored in green.')
+    p.add_argument('--blue', nargs=3, type=int,
+                   help='MI-Brain voxels coordinates of the third experiment.\n'
+                        'Third experiment will be colored in blue.')
+    g = p.add_mutually_exclusive_group(required=True)
+    g.add_argument('--injection', action="store_true",
+                   help='Use `experiment_injection_coordinate_search` to find experiments.\n'
+                        'https://allensdk.readthedocs.io/en/latest/'
+                        'allensdk.api.queries.mouse_connectivity_api.html')
+    g.add_argument('--spatial', action="store_true",
+                   help='Use `experiment_spatial_search` to find experiments.\n'
+                        'https://allensdk.readthedocs.io/en/latest/'
+                        'allensdk.api.queries.mouse_connectivity_api.html')
+    p.add_argument('--threshold', type=float, default=0.30,
+                   help='Combined projection density threshold for finding '
+                        'masks of crossing ROIs.\n'
+                        'Threshold is 0.30 by default.\n'
+                        '--threshold <value> will set threshold to value.')
+    p.add_argument('-r', '--res', type=int, default=100, choices=[25, 50, 100],
+                   help='Resolution is 100µm by default.\n'
+                        '--res <value> will set the resolution to value.')
+    p.add_argument('-d', '--dir', default=".",
+                   help='Path of the ouptut file directory is . by default.\n'
+                        '--dir <dir> will change the output file\'s '
+                        'directory or create a new one if does not exits.')
+    p.add_argument('-f', dest='overwrite', action="store_true",
+                   help='Force overwriting of the output file.')
+    p.add_argument('-c', '--nocache', action="store_true",
+                   help='Update the Allen Mouse Brain Connectivity Cache')
+    return p
+
+
+def main():
+    # Building argparser
+    parser = _build_arg_parser()
+    args = parser.parse_args()
+
+    print(args.red, args.green, args.threshold, args.injection, args.spatial)
+
+
+if __name__ == "__main__":
+    main()
