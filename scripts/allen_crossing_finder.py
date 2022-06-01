46c2f249e2fb3fd2a743f2437ca29a5fd90833db
Mahdi <mah.abha8@gmail.com>
2022-06-01 10:33:22 +0200

    issue#51 : verification of coordinates values

--- scripts/allen_crossing_finder.py
+++ scripts/allen_crossing_finder.py
@@ -28,25 +28,28 @@ EPILOG = """
 Author : Mahdi
 """
 
+
 def _build_arg_parser():
     p = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
                                 epilog=EPILOG, description=__doc__)
     p.add_argument('--red', nargs=3, type=int, required=True,
-                   help='MI-Brain voxels coordinates of the first experiment.\n'
+                   help='MI-Brain voxels coordinates of first experiment.\n'
                         'First experiment will be colored in red.')
     p.add_argument('--green', nargs=3, type=int, required=True,
-                   help='MI-Brain voxels coordinates of the second experiment.\n'
+                   help='MI-Brain voxels coordinates of second experiment.\n'
                         'Second experiment will be colored in green.')
     p.add_argument('--blue', nargs=3, type=int,
-                   help='MI-Brain voxels coordinates of the third experiment.\n'
+                   help='MI-Brain voxels coordinates of third experiment.\n'
                         'Third experiment will be colored in blue.')
     g = p.add_mutually_exclusive_group(required=True)
     g.add_argument('--injection', action="store_true",
-                   help='Use `experiment_injection_coordinate_search` to find experiments.\n'
+                   help='Use `experiment_injection_coordinate_search` '
+                        'to find experiments.\n'
                         'https://allensdk.readthedocs.io/en/latest/'
                         'allensdk.api.queries.mouse_connectivity_api.html')
     g.add_argument('--spatial', action="store_true",
-                   help='Use `experiment_spatial_search` to find experiments.\n'
+                   help='Use `experiment_spatial_search` '
+                        'to find experiments.\n'
                         'https://allensdk.readthedocs.io/en/latest/'
                         'allensdk.api.queries.mouse_connectivity_api.html')
     p.add_argument('--threshold', type=float, default=0.30,
@@ -68,12 +71,82 @@ def _build_arg_parser():
     return p
 
 
+def check_args(parser, args):
+    """
+    Verify that the arguments are called the right way
+
+    Parameters
+    ----------
+    parser: argparse.ArgumentParser object
+        Parser.
+    args: argparse namespace
+        Argument list.
+    """
+    if 1.0 < args.threshold < 0.0:
+        parser.error('Please enter a valid threshold value. '
+                     'Pick a float value from 0.0 to 1.0')
+
+    x, y, z = range(0, 164), range(0, 212), range(0, 158)
+
+    if args.red[0] not in x or args.red[1] not in y or args.red[2] not in z:
+        parser.error('Red coords invalid. '
+                     'x, y, z values must be in [164, 212, 158].')
+
+    if args.green[0] not in x or args.green[1] not in y or args.green[2] not in z:
+        parser.error('Green coords invalid. '
+                     'x, y, z values must be in [164, 212, 158].')
+
+    if args.blue:
+        if args.blue[0] not in x or args.blue[1] not in y or args.blue[2] not in z:
+            parser.error('Blue coords invalid. '
+                         'x, y, z values must be in [164, 212, 158].')
+
+
+def check_file_exists(parser, args, path):
+    """
+    Verify that output does not exist or that if it exists, -f should be used.
+    If not used, print parser's usage and exit.
+
+    Parameters
+    ----------
+    parser: argparse.ArgumentParser object
+        Parser.
+    args: argparse namespace
+        Argument list.
+    path: string or path to file
+        Required path to be checked.
+    """
+    if os.path.isfile(path) and not args.overwrite:
+        parser.error('Output file {} exists. Use -f to force '
+                     'overwriting'.format(path))
+
+    path_dir = os.path.dirname(path)
+    if path_dir and not os.path.isdir(path_dir):
+        parser.error('Directory {}/ \n for a given output file '
+                     'does not exists.'.format(path_dir))
+
+
 def main():
     # Building argparser
     parser = _build_arg_parser()
     args = parser.parse_args()
 
-    print(args.red, args.green, args.threshold, args.injection, args.spatial)
+    # Verying args validity
+    check_args(parser, args)
+
+    # Getting allen coords
+
+    # Finding experiments
+        # if --spatial
+        # if --injection
+    
+    # Aligning and saving projection density volumes
+
+    # Creating and saving RGB volume 
+
+    # Searching crossing regions
+
+    # Creating and saving ROI mask
 
 
 if __name__ == "__main__":
