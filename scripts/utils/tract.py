0fdd8d4f2a03ce4ca79e18e8a151c58aaf4a29ad
Mahdi <mah.abha8@gmail.com>
2022-06-24 12:25:53 +0200

    #75 doc module tract

--- scripts/utils/tract.py
+++ scripts/utils/tract.py
@@ -7,6 +7,7 @@ from utils.util import load_avgt
 
 def get_allen_tract():
     """
+    Load avgt_wildtype_tractogram.trk
     """
     fname = "./data/avgt/avgt_wildtype_tractogram.trk"
     return load_tractogram(fname)
@@ -14,35 +15,51 @@ def get_allen_tract():
 
 def get_streamlines():
     """
+    Get streamlines datas of the tractogram
     """
     return get_allen_tract()[0]
 
 
 def get_header():
     """
+    Get metadatas of the tractogram
     """
     return get_allen_tract()[1]
 
 
 def filter_tract_near_roi(mask, fname):
     """
+    Save a bundle of streamlines passing
+    through the binary mask
+
+    Parameters
+    ----------
+    mask: ndarray
+        Binary mask.
+    fname: string
+        Path to the output file
     """
+    # Getting streamlines of the avgt allen tractogram
     streamlines = get_streamlines()
 
-    nearby_roi = near_roi(
+    # Filtering streamlines
+    # Keeping only the ones that pass through the roi
+    through_roi = near_roi(
         streamlines=streamlines,
         affine=load_avgt().affine,
         region_of_interest=mask
     )
 
-    streamlines_near_roi = []
+    # Writting streamlines array sequence
+    streamlines_through_roi = []
     for i in range(len(streamlines)):
-        if nearby_roi[i]:
-            streamlines_near_roi.append(streamlines[i])
-    
+        if through_roi[i]:
+            streamlines_through_roi.append(streamlines[i])
+
+    # Saving the tractogram
     save_tractogram(
         fname=fname,
-        streamlines=streamlines_near_roi,
+        streamlines=streamlines_through_roi,
         affine=np.eye(4),
         header=get_header()
     )
