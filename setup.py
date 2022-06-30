d015fad9bb7ceaba5b38b786878795ad3e2729b7
Mahdi <mah.abha8@gmail.com>
2022-06-30 10:38:58 +0200

    like scilpy

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 
 from setuptools import setup, find_packages
-
+from setuptools.command.build_ext import build_ext
 
 with open('a2t_requirements.txt') as f:
     required_dependencies = f.read().splitlines()
@@ -13,6 +13,26 @@ with open('a2t_requirements.txt') as f:
             external_dependencies.append('{} @ {}'.format(repo_name, repo_url))
         else:
             external_dependencies.append(dependency)
+
+
+class CustomBuildExtCommand(build_ext):
+    """ build_ext command to use when numpy headers are needed. """
+
+    def run(self):
+        # Now that the requirements are installed, get everything from numpy
+        from Cython.Build import cythonize
+        from numpy import get_include
+
+        # Add everything requires for build
+        self.swig_opts = None
+        self.include_dirs = [get_include()]
+        self.distribution.ext_modules[:] = cythonize(
+            self.distribution.ext_modules)
+
+        # Call original build_ext command
+        build_ext.finalize_options(self)
+        build_ext.run(self)
+
 
 # Get version and release info, which is all stored in module/version.py
 ver_file = os.path.join('allen2tract', 'version.py')
@@ -32,7 +52,7 @@ opts = dict(name=NAME,
             platforms=PLATFORMS,
             version=VERSION,
             packages=find_packages(),
-            setup_requires=['numpy'],
+            setup_requires=['cython','numpy'],
             install_requires=external_dependencies,
             scripts=SCRIPTS,
             include_package_data=True)
