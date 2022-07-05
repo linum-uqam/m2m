cabf51e2f852e348ad03e466b0a5b4da4e9c733b
Mahdi <mah.abha8@gmail.com>
2022-07-05 13:37:32 +0200

    can import ids as csv file from allen brain

--- scripts/allen_import_tract.py
+++ scripts/allen_import_tract.py
@@ -5,14 +5,16 @@
     Download all streamlines for each Allen mouse brain
     connectivity atlas experiments and combine into a single tractogram
 
-    Please use allen_tract_transform.py to align your tractogram on your
-    reference before using it in other scripts.
+    Note: The output tract will be aligned on the allen ras template.\n
+          Please use allen_tract_transform.py to align your tractogram on your
+          reference before using it in other scripts.
 
-    >>> allen_import_tract id1 id2 id3 id4 ... --dir dir
+    >>> allen_import_tract path/to/ids.csv path/to/output.trk
    """
 
 import argparse
 import numpy as np
+import pandas as pd
 from pathlib import Path
 import os
 
@@ -35,9 +37,8 @@ Author : Mahdi
 def _build_arg_parser():
     p = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
                                 epilog=EPILOG, description=__doc__)
-    p.add_argument('ids', type=int, nargs='+',
-                   help='Experiment ids in the Allen Mouse Brain '
-                        'Connectivity Atlas dataset.')
+    p.add_argument('in_ids', help='Path to a csv file containing ids')
+    p.add_argument('out_tract', help='Path to output tractogram (trk)')
     add_output_dir_arg(p)
     add_overwrite_arg(p)
     add_cache_arg(p)
@@ -56,32 +57,27 @@ def main():
     # Configuring cache dir
     cache_dir = Path().home() / "allen2tract/data"
 
+    # Verifying output file
+    check_file_exists(parser, args, args.out_tract)
+    in_ids = pd.read_csv(args.in_ids).id.tolist()
+
     # Getting allen experiments
     allen_experiments = get_mcc(args)[0]
 
     # Verifying experiment id
     ids = allen_experiments.id
-    for id in args.ids:
+    for id in in_ids:
         if id not in ids:
             parser.error("Experiment {} doesn't exist.\n"
                          "Please check: https://connectivity.brain-map.org/"
                          "".format(id))
 
-    # Preparing filename
-    str_ids = str(args.ids[0])
-    if len(args.ids) > 1:
-        for i in range(1, len(args.ids)):
-            str_ids += "-{}".format(args.ids[i])
-    trk_ = "{}.trk".format(str_ids)
-    trk_file = os.path.join(args.dir, trk_)
-    check_file_exists(parser, args, trk_file)
-
     # Initializing and downloading the streamlines
     s = AllenStreamLines(cache_dir / "cache_streamlines")
-    s.download(args.ids)
+    s.download(in_ids)
 
     # Save the streamlines as a .trk file
-    s.download_tract(trk_file)
+    s.download_tract(args.out_tract)
 
 
 if __name__ == "__main__":
