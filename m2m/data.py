6c75d9c698727d42250705072914cd8b678480f3
Joël Lefebvre <lefebvre.joel@uqam.ca>
2023-01-24 17:14:41 -0500

    Adding documentation to the function

--- m2m/data.py
+++ m2m/data.py
@@ -1,7 +1,4 @@
 import gdown
-import os
-import requests
-import shutil
 from pathlib import Path
 
 from m2m.control import get_cached_dir
@@ -13,20 +10,25 @@ data_ids = {
     "allen_template_ras_50um.nii.gz": "1hX_oW8TLH6aPMvdk7-fuikeuJhKIASa2",
 }
 
-def download_to_cache(data_id: str, force_download: bool=False, dry_run=False) -> str:
-    """ Download the Allen Mouse Brain Tractogram
+
+def download_to_cache(data_id: str, force_download: bool = False) -> str:
+    """
+    Download data to cache
+
+    Parameters
+    ----------
+    data_id
+        Data to download. The available are the keys of data.data_ids dictionary
+    force_download
+        Force the download of the file if it is already in the cache
 
     Returns
     -------
-    Filename
-
+    Full path to the downloaded file
     """
     assert data_id in data_ids.keys(), f"Available data: {data_ids.keys()}"
     url = GDRIVE_BASE_URL.format(data_ids[data_id])
     local_filename = Path(get_cached_dir("data")) / data_id
-
-    if dry_run:
-        return str(local_filename)
 
     if not local_filename.is_file() or force_download:
         local_filename.parent.mkdir(exist_ok=True, parents=True)
