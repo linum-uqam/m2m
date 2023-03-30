c8a8e796603087c8991f242b1b31c39943ecae27
Abou-Hamdan, Mahdi Mickaël <abou-hamdan.mahdi_mickael@courrier.uqam.ca>
2023-03-30 18:19:07 -0400

    working version for wsl

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # For more information, please refer to https://aka.ms/vscode-docker-python
-FROM python:3.7-slim
+FROM python:3.9-slim
 
 # Keeps Python from generating .pyc files in the container
 ENV PYTHONDONTWRITEBYTECODE=1
@@ -7,12 +7,26 @@ ENV PYTHONDONTWRITEBYTECODE=1
 # Turns off buffering for easier container logging
 ENV PYTHONUNBUFFERED=1
 
-# Install pip requirements
-COPY requirements.txt .
-RUN python -m pip install -r requirements.txt
-
+# Workdir
 WORKDIR /app
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    curl \
+    software-properties-common \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
 COPY . /app
+# or 
+# RUN git clone https://github.com/linum-uqam/m2m .
 
 # Install the module
-RUN python -m pip install -e .
+RUN pip install -r requirements.txt
+RUN pip install -e .
+
+EXPOSE 8501
+
+HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
+
+ENTRYPOINT ["streamlit", "run", "app/m2m_main_page.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- app/pages/m2m_compute_transform_matrix_page.py
+++ app/pages/m2m_compute_transform_matrix_page.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import subprocess
+import os
 
 st.title('Compute Affine Transformation Matrix')
 
@@ -7,20 +8,21 @@ st.title('Compute Affine Transformation Matrix')
 st.write('https://m2m.readthedocs.io/en/latest/scripts/m2m_compute_transform_matrix.html')
 
 # Reference Image Path
+#ref_path = st.file_uploader('Upload reference Image (.nii/.nii.gz)', type=([".nii",".nii.gz"]))
 ref_path = st.text_input('Enter reference Image Path (/path/to/reference.nii.gz)')
 
 # Output Matrix Path
 out_path = st.text_input('Enter output Matrix Path (/path/to/output.mat)')
 
 # Resolution
-resolution = st.selectbox('Select Allen resolution (microns)', [25, 50, 100])
+allen_res = st.radio('Select Allen resolution (microns)', [25, 50, 100])
 
 # User Resolution
-user_res = st.number_input('Enter User resolution', value=0, step=1)
+user_res = st.number_input('Enter User resolution (microns)', value=0, step=1)
 
 # Button to run script
 if st.button('Compute Affine Transformation Matrix'):
-    command = ['python', 'scripts/m2m_compute_transform_matrix.py', ref_path, out_path, str(resolution), str(user_res)]
+    command = ['python3', 'scripts/m2m_compute_transform_matrix.py', str(ref_path), str(out_path), str(allen_res), str(user_res)]
     try:
         result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         st.success(result.stdout)
