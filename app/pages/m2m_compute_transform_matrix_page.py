76ea71c0c6d7dceadf5b63db8d20c78eaaf45856
Abou-Hamdan, Mahdi Mickaël <abou-hamdan.mahdi_mickael@courrier.uqam.ca>
2023-03-30 19:31:46 -0400

    added 2 interface (not tested yet)

--- app/pages/m2m_compute_transform_matrix_page.py
+++ app/pages/m2m_compute_transform_matrix_page.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import subprocess
-import os
+from pathlib import Path
 
 st.title('Compute Affine Transformation Matrix')
 
 # Link to the documentation
 st.write('https://m2m.readthedocs.io/en/latest/scripts/m2m_compute_transform_matrix.html')
+st.write("Please upload the input files and set the arguments for the script:")
 
 # Reference Image Path
 #ref_path = st.file_uploader('Upload reference Image (.nii/.nii.gz)', type=([".nii",".nii.gz"]))
-ref_path = st.text_input('Enter reference Image Path (/path/to/reference.nii.gz)')
+ref = st.file_uploader("Reference file (nifti):", type=["nii", "nii.gz"])
+if ref :
+    ref_path = Path(ref.name)
+    ref_path.write_bytes(ref.getvalue())
 
 # Output Matrix Path
 out_path = st.text_input('Enter output Matrix Path (/path/to/output.mat)')
@@ -25,6 +29,6 @@ if st.button('Compute Affine Transformation Matrix'):
     command = ['python3', 'scripts/m2m_compute_transform_matrix.py', str(ref_path), str(out_path), str(allen_res), str(user_res)]
     try:
         result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        st.success(result.stdout)
+        st.success("Matrix {} downloaded successfully".format(out_path))
     except subprocess.CalledProcessError as e:
         st.error(e.stderr)
