422a27e82ebd8b688ea4eabb35e06baa7b40b1b1
Abou-Hamdan, Mahdi Mickaël <abou-hamdan.mahdi_mickael@courrier.uqam.ca>
2023-05-04 12:34:26 -0400

    donwload button successfull

--- app/pages/m2m_compute_transform_matrix_page.py
+++ app/pages/m2m_compute_transform_matrix_page.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import subprocess
+import scipy.io as sio
+import tempfile
+import sys
 from pathlib import Path
 
 # Page configuration
@@ -11,34 +14,46 @@ st.title('Compute Affine Transformation Matrix')
 st.write('https://m2m.readthedocs.io/en/latest/scripts/m2m_compute_transform_matrix.html')
 st.write("Please upload the input files and set the arguments for the script:")
 
-# Reference Image Path
-#ref_path = st.file_uploader('Upload reference Image (.nii/.nii.gz)', type=([".nii",".nii.gz"]))
+# Reference image
 ref = st.file_uploader("Reference file (nifti):", type=["nii", "nii.gz"])
-if ref :
+if ref:
     ref_path = Path(ref.name)
-    ref_path.write_bytes(ref.getvalue())
+    ref_bytes = ref.getvalue()
 
-# Output Matrix Path
-out_path = st.text_input('Enter output Matrix Path (/path/to/output.mat)')
-
-# Set override
-override = st.checkbox("Overwrite output file")
+# Output matrix
+out_mat = st.text_input('Enter output matrix name (without file extension)')
+mat = "{}.mat".format(out_mat)
 
 # Resolution
-allen_res = st.radio('Select Allen resolution (microns)', [25, 50, 100])
+allen_res = st.radio('Select allen resolution (microns)', [25, 50, 100])
 
-# User Resolution
-user_res = st.number_input('Enter User resolution (microns)', value=0, step=1)
+# User resolution
+user_res = st.number_input('Enter user resolution (microns)', value=0, step=1)
 
-# Button to run script
+# Run script
 if st.button('Compute Affine Transformation Matrix'):
-    command = ['python3', 'scripts/m2m_compute_transform_matrix.py', str(ref_path), str(out_path), str(allen_res), str(user_res)]
-    if override:
-        command.append('-f')
-    try:
-        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        st.success("Matrix {} downloaded successfully".format(out_path))
-        ref_path.unlink()
-    except subprocess.CalledProcessError as e:
-        st.error(e.stderr)
-        ref_path.unlink()
+    with tempfile.TemporaryDirectory() as tempdir:
+        # Save the reference image in the tempdir
+        ref_path = Path(tempdir) / ref_path.name
+        with open(ref_path, 'wb') as f:
+            f.write(ref_bytes)
+
+        # Build the command to run the script
+        command = ['python3', 'scripts/m2m_compute_transform_matrix.py', str(ref_path), str(Path(tempdir) / mat), str(allen_res), str(user_res)]
+
+        # Run the command
+        try:
+            result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+            st.success("Matrix {} computation completed successfully".format(mat))
+
+            # Download the output matrix
+            with open(Path(tempdir) / mat, 'rb') as f:
+                matrix_bytes = f.read()
+            st.download_button(
+                label="Download matrix",
+                data=matrix_bytes,
+                file_name=mat,
+                mime="application/octet-stream"
+            )
+        except subprocess.CalledProcessError as e:
+            st.error(e.stderr)
