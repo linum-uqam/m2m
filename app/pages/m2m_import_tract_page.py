fe083c47ae5b3ca5dca838cbd625b4b3c818cee9
Abou-Hamdan, Mahdi Mickaël <abou-hamdan.mahdi_mickael@courrier.uqam.ca>
2023-07-10 14:22:31 -0400

    lighten import tract  interface

--- app/pages/m2m_import_tract_page.py
+++ app/pages/m2m_import_tract_page.py
@@ -13,21 +13,17 @@ st.title('M2M Import Tract')
 st.write('https://m2m.readthedocs.io/en/latest/scripts/m2m_import_tract.html')
 st.write('Download streamlines from Allen Mouse Brain Connectivity Atlas and combine them into a single tractogram.')
 
-# Step 1: Set Output Tractogram Path
-st.subheader('Step 1: Set Output Tractogram Path')
-out_tract = st.text_input('Path to output tractogram (trk)')
-
-# Step 2: Upload reference file and matrix file
-st.subheader('Step 2: Upload reference file and matrix file') 
+# Step 1: Upload reference file and matrix file
+st.subheader('Step 1: Upload reference file and matrix file') 
 ref = st.file_uploader("Reference file (nifti):", type=["nii", "nii.gz"])
 mat = st.file_uploader("Matrix file (mat):", type=["mat"])
 
-# Step 3: Select Allen resolution
-st.subheader('Step 3: Select resolution)')
+# Step 2: Select Allen resolution
+st.subheader('Step 2: Select resolution)')
 res = st.radio('Resolution', [25, 50, 100])
 
-# Step 4: Set Experiment IDs
-st.subheader('Step 4: Set Experiment IDs')
+# Step34: Set Experiment IDs
+st.subheader('Step 3: Set Experiment IDs')
 ids_type = st.radio('IDs Type', ['CSV File', 'Manual'])
 if ids_type == 'CSV File':
     ids_csv = st.file_uploader('CSV File', type=['csv'])
@@ -37,8 +33,8 @@ if ids_type == 'CSV File':
 else:
     in_ids = st.text_input('Experiment IDs (separated by spaces)').split()
 
-# Step 5: Run Script
-st.subheader('Step 5: Run Script')
+# Step 4: Run Script
+st.subheader('Step 4: Run Script')
 if st.button('Run'):
     with tempfile.TemporaryDirectory() as tempdir:
         # Save the reference image and the matrix in the tempdir
@@ -48,6 +44,7 @@ if st.button('Run'):
         mat_path = Path(tempdir) / Path(mat.name).name
         with open(mat_path, 'wb') as f:
             f.write(mat.getvalue())
+        out_tract = "tract.trk"
         cmd = ['python3', 'scripts/m2m_import_tract.py', str(out_tract), str(mat_path), str(ref_path), str(res)]
         if ids_type == 'CSV File':
             cmd.extend(['--ids_csv', str(ids_csv.name)])
@@ -57,7 +54,7 @@ if st.button('Run'):
             result = subprocess.run(cmd, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             st.success("Tract imported successfully")
             # Download the output tract
-            st.subheader('Step 6: Download tract')
+            st.subheader('Step 5: Download tract')
             with open(Path(tempdir) / out_tract, 'rb') as f:
                 trk_bytes = f.read()
                 st.download_button(
